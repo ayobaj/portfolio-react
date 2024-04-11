f37772035bb44dfce41f1b76d8ceb22c5149309c
ayo baj <ayobaj007@gmail.com>
2024-04-11 14:42:41 +0100

    google drive

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -9,17 +9,24 @@ const Resume = () => {
     };
 
     return (
-        <div className="text-2xl text-center flex flex-col pt-9 space-y-[290px]  h-full ">
+        <div className="text-2xl text-center flex flex-col pt-9 space-y-[290px] justify-center items-center h-full ">
             
             <h1 className="font-bold"> RESUME</h1>
             
-            <button onClick={downloadResume} className=" flex items-center justify-center ml-5 mr-5 pt-2 pb-3  bg-gradient-to-r  bg-blue-950 text-white hover:from-blue-500 hover: to-blue-950 focus:outline-none focus:ring focus:ring-blue-100
-            active:bg-blue-700 rounded-lg font-bold shadow-md transition duration-300 ease-in-out transform hover:scale-105 ">Download<span><MdSimCardDownload/></span>
-            </button>
+            <a href="https://drive.google.com/file/d/1jh5qY8HQxqG-akcfBIEPsf888O1qpVu9/view?usp=sharing">
+                <button className=" py-2 px-4 flex items-center bg-gradient-to-r  bg-blue-950 text-white hover:from-blue-500 hover: to-blue-950 focus:outline-none focus:ring focus:ring-blue-100
+                active:bg-blue-700 rounded-lg font-bold shadow-md transition duration-300 ease-in-out transform hover:scale-105 ">Google Drive<span><MdSimCardDownload/></span>
+                </button>
+            </a>
+            
             
             <button className="flex justify-center tilt">
                 <FaHandPeace className="text-4xl mb-3"/>
             </button>
+
+
+            
+
             
         </div>
     );
