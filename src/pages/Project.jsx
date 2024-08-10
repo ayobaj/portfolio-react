2846dd661f7337ec63de09eb73876ddc74169e9d
ayo baj <ayobaj007@gmail.com>
2024-08-10 11:10:00 +0100

    feat: added the image for the new project and filled the right word for the tech stack

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -1,6 +1,7 @@
 import pfl from "../PreImg/pfl.png"
 import wes from "../PreImg/wes.png"
 import bajz from "../PreImg/bajz.png"
+import medisched from "../PreImg/medi-sched.png"
 
 
 const Project = () => {
@@ -10,6 +11,42 @@ return (
 
         {/* first project */}
 
+        <div className=" flex flex-col lg:flex-row items-center space-y-5 lg:space-x-8 lg:mt-4 lg:ml-6 pb-6 text-reveal">
+            
+            <div>
+                <img className="w-[300px] h-[280px] rounded-md mt-6 object-contain" src={medisched}></img>   
+            </div>
+
+            <div className="space-y-4">
+                
+                <div className="">
+                    <h1 className="text-xl lg:text-2xl text-center font-bold lg:mr-[290px]">BAJZ</h1>
+                </div>
+
+                <div className="px-7 md:px-7">
+                    <h3><span className="text-xl font-bold">Tech Stack:</span> Next.JS, typescript, tailwind, shadcn-ui, twilio and sentry</h3>
+                    <p>A fullstack health management web application developed to curb the tedious process of booking an appointment with a doctor . </p>
+                </div>
+
+                <div className="animate-text-left-to-right flex mr-[150px] md:mr-[262px] space-x-3 ml-7 md:ml-6">
+                    <a href="https://bajz.onrender.com/" className="buttons">
+                    <button className="w-[100px] pt-2 pb-3 bg-gradient-to-r  bg-blue-950 text-white hover:from-blue-500 hover:to-blue-950 focus:outline-none focus:ring focus:ring-blue-100 active:bg-blue-700 rounded-lg font-bold shadow-md transition duration-300 ease-in-out transform hover:scale-105">
+                        Live
+                    </button>
+                    </a>
+
+                    <a href="https://github.com/ayobaj/BAJZ" className="buttons">
+                    <button className="w-[100px] pt-2 pb-3 bg-gradient-to-r  bg-blue-950 text-white hover:from-blue-500 hover:to-blue-950 focus:outline-none focus:ring focus:ring-blue-100 active:bg-blue-700 rounded-lg font-bold shadow-md transition duration-300 ease-in-out transform hover:scale-105">
+                        GitHub
+                    </button>
+                    </a>
+                </div>
+                
+            </div>
+
+        </div>
+
+        {/*2nd project*/}
         <div className=" flex flex-col lg:flex-row items-center space-y-5 lg:space-x-8 lg:mt-4 lg:ml-6 pb-6 text-reveal">
             
             <div>
