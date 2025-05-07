4c9a2e9314a56a2eaedda6d83f17055fb505347b
ayobaj <ayobaj007@gmail.com>
2025-05-07 19:12:09 -0400

    feat: word change "front eengineer" to "software engineer" on the preloader page

--- src/components/PreLoader.jsx
+++ src/components/PreLoader.jsx
@@ -12,7 +12,7 @@ const PreLoader = () => {
         <div className=" preloader">
             <img className="rounded-full" src={logo} height={100} width={80} alt="PreImg" />
             <div className="texts-container"> 
-            <span>Frontend Engineer</span>
+            <span>Software Engineer</span>
             <span>FullStack Engineer</span>
             <span>Graphics Designer</span>
             </div>
